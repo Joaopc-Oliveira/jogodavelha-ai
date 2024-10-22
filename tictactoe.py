931f48a1c3b07ba9ed98f2f1e33a8997a315bad4
João Oliveira <joaopaulocamara1@hotmail.com>
2024-10-22 23:23:28 +0100

    Adiciona versão básica do jogo da velha com funções iniciais

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,6 @@
-"""
-Tic Tac Toe Player
-"""
-
-import math
-
 X = "X"
 O = "O"
 EMPTY = None
-
 
 def initial_state():
     """
@@ -17,51 +10,28 @@ def initial_state():
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
 
-
 def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
-
+    x_count = sum(row.count(X) for row in board)
+    o_count = sum(row.count(O) for row in board)
+    return X if x_count <= o_count else O
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
-
+    return {(i, j) for i in range(3) for j in range(3) if board[i][j] == EMPTY}
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    i, j = action
+    if board[i][j] is not EMPTY:
+        raise ValueError("Invalid action: cell is already occupied")
 
-
-def winner(board):
-    """
-    Returns the winner of the game, if there is one.
-    """
-    raise NotImplementedError
-
-
-def terminal(board):
-    """
-    Returns True if game is over, False otherwise.
-    """
-    raise NotImplementedError
-
-
-def utility(board):
-    """
-    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
-    """
-    raise NotImplementedError
-
-
-def minimax(board):
-    """
-    Returns the optimal action for the current player on the board.
-    """
-    raise NotImplementedError
+    new_board = [row[:] for row in board]
+    new_board[i][j] = player(board)
+    return new_board
